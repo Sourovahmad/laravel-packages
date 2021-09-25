1d2100cdae03513e4d3fd3910492111f567187e1
sourovahmad <mdsourovahmad19@gmail.com>
2021-09-25 13:29:29 +0600

    unzipper package done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Sourovahmad\Unzipper\Facades\SourovahmadUnzipper;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function()
+{
+  return view('welcome');
+});
+
+    // SourovahmadUnzipper::unzip('hello');
+
+
+Route::post('submitForm', function (Request $request) {
+
+
+     SourovahmadUnzipper::unzip($request->file);
+
+
+
+})->name('submitForm');
+
+
+
+Route::get('getData', function()
+{
+    SourovahmadUnzipper::getZipData('1632553485');
 });
